f318d3bf35200fe2fd4b41333004c56ac9e9ee4a
efoosag <96612103+efoosag@users.noreply.github.com>
2022-07-13 19:57:57 +0100

    update ReadMe File

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,9 +3,12 @@ import React from 'react';
 
 const Quote = () => (
   <section className="home-section quote" id="quote">
-    "Electronic calculators can solve problems which the man who made them
-    cannot solve; but no government-subsidized commission of engineers and
-    physicists could create a worm." - "Joseph Wood Krutch"
+    <h2 className="quote-title">Quote</h2>
+    <p>
+      "Electronic calculators can solve problems which the man who made them
+      cannot solve; but no government-subsidized commission of engineers and
+      physicists could create a worm." - "Joseph Wood Krutch"
+    </p>
   </section>
 );
 
